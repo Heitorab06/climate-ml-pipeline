20f6f9b7199d2431c45454fe602060de2a400913
Heitor Barreto <habarreto06@gmail.com>
2026-08-12 12:12:26 -0300

    Primeiro request

--- src/extraction/api.py
+++ src/extraction/api.py
@@ -3,6 +3,19 @@ import pandas
 
 url = "https://archive-api.open-meteo.com/v1/archive"
 
-r = requests.get(url=url)
 lat = -12.9822499
 long = -38.4812772
+data = ["temperature_2m"]
+
+params = {
+    "latitude": lat,
+    "longitude": long,
+    "hourly": data
+}
+
+r = requests.get(url=url, params=params)
+
+json = r.json()
+
+for key in json:
+    print(key)
