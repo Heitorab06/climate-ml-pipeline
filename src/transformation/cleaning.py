a95f061d77cf8c203aff2d5c1dc23bcebb52cd84
Heitor Barreto <habarreto06@gmail.com>
2026-08-15 14:21:20 -0300

    Adicionando função para converter tipos

--- src/transformation/cleaning.py
+++ src/transformation/cleaning.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from src.extraction.api import get_weather_data
+from datetime import datetime
 
 df = pd.read_parquet("data/weather_raw.parquet")
 
-print(df.head())
+def convert_data_types(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    
+    format_date = "%Y-%m-%dT%H:%M"
+    
+    df["time"] = pd.to_datetime(df["time"], format=format_date)
+         
+    return df
+
+
+df_clean = convert_data_types(df=df)
+df_clean.info()
