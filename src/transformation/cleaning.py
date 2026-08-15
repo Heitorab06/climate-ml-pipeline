bf14e3c83f6324d0ee21f6617f7c39dad124d261
Heitor Barreto <habarreto06@gmail.com>
2026-08-15 14:57:09 -0300

    Adicionando função pipeline na limpeza dos dados

--- src/transformation/cleaning.py
+++ src/transformation/cleaning.py
@@ -2,15 +2,21 @@ import pandas as pd
 
 df = pd.read_parquet("data/weather_raw.parquet")
 
-def convert_data_types(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    return (df.copy()
+            .pipe(convert_data_types)
+            .pipe(set_index_time))
     
+def convert_data_types(df: pd.DataFrame) -> pd.DataFrame:    
     format_date = "%Y-%m-%dT%H:%M"
     
     df["time"] = pd.to_datetime(df["time"], format=format_date)
          
     return df
 
+def set_index_time(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.set_index("time")
+    return df
 
-df_clean = convert_data_types(df=df)
-df_clean.info()
+df = clean_data(df=df)
+
