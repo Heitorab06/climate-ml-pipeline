7bcddf23d0ef684747f89e036aa881eeb2bae5f1
Heitor Barreto <habarreto06@gmail.com>
2026-08-18 09:59:06 -0300

    Criando Funções para configurar o database

--- src/loading/database.py
+++ src/loading/database.py
@@ -1,27 +1,39 @@
 import os
 
-import psycopg2
+import pandas as pd
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
 load_dotenv()
 
-db_host = os.getenv("HOST")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-port = os.getenv("PORT")
-db_name = os.getenv("DB_NAME")
+def connect() -> Engine:
+    db_host = os.getenv("HOST")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    port = os.getenv("PORT")
+    db_name = os.getenv("DB_NAME")
 
-print("Diretório atual de execução:", os.getcwd())
-print("HOST lido:", repr(os.getenv("HOST")))
-print("USER lido:", repr(os.getenv("DB_USER")))
-print("PASSWORD lida:", repr(os.getenv("DB_PASSWORD")))
-
-conn = psycopg2.connect(
-    host=db_host,
-    dbname=db_name,
-    port=port,
-    user=db_user,
-    password=db_password)
+    conn_string = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{port}/{db_name}"
+    engine = create_engine(conn_string)
+    return engine
 
 
-conn.close()
+def upload_to_postgres() -> None:
+    engine = connect()
+    df = pd.read_parquet("data/weather_clean.parquet")
+    
+    df.to_sql(
+        name = "weather",
+        con = engine,
+        if_exists="replace",
+        index=True,
+        index_label="time"
+    )
+
+
+def select(query:str) -> pd.DataFrame:
+    engine = connect()
+    df = pd.read_sql(query, con=engine)
+    return df
+
