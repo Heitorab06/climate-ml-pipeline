a0b7884d99d2bba4bab8401bac21023f48fb393d
Heitor Barreto <habarreto06@gmail.com>
2026-08-21 18:50:57 -0300

    feat(modelling): create_lag_features

--- src/modeling/feature_engineering.py
+++ src/modeling/feature_engineering.py
@@ -5,8 +5,17 @@ df = select("SELECT * FROM WEATHER")
 print(df.columns)
 
 def create_lag_features(df:pd.DataFrame, cols: list[str], lags: list[int]) -> pd.DataFrame:
+    for col in cols:
+        for lag in lags:
+            df[f"{col}_lag_{lag}"] = df[col].shift(lag)
+        
+        if col == "precipitation":
+            df[f"cumulative_{col}_3h"] = df[{col}].rolling(3).sum()
+        else: 
+            df[f"{col}_variation_3h"] = df[col] - df[col].shift(3)
     
     return df
+
 
 def create_time_cyclical_features(df:pd.DataFrame) -> pd.DataFrame:
     
@@ -18,6 +27,7 @@ def create_target(df:pd.DataFrame) -> pd.DataFrame:
 
 def run_feature_engineering(df:pd.DataFrame)-> pd.DataFrame:
     lag_features = ["temperature_2m", "relative_humidity_2m", "pressure_msl", "wind_speed_10m", "precipitation"]
+    
     return (df.copy()
             .pipe(create_lag_features(cols=lag_features, lags=[1,2,3,6]))
             .pipe(create_time_cyclical_features)
