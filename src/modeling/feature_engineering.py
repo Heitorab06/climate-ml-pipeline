6f16800b869bb3d5555f28f9e16833544b79272f
Heitor Barreto <habarreto06@gmail.com>
2026-08-21 19:23:18 -0300

    feat(modelling): feature_engineering completa

--- src/modeling/feature_engineering.py
+++ src/modeling/feature_engineering.py
@@ -20,15 +20,17 @@ def create_lag_features(df:pd.DataFrame, cols: list[str], lags: list[int]) -> pd
 
 def create_time_cyclical_features(df:pd.DataFrame) -> pd.DataFrame:
     
-    df["hour_sin"] = np.sin(np.pi * df["time"].dt.hour/24)
-    df["hour_cos"] = np.cos(np.pi * df["time"].dt.hour/24)
+    df["hour_sin"] = np.sin(2 * np.pi * df["time"].dt.hour/24)
+    df["hour_cos"] = np.cos(2 * np.pi * df["time"].dt.hour/24)
     
-    df["day_sin"] = np.sin(np.pi * df["time"].dt.dayofyear/365)
-    df["day_cos"] = np.cos(np.pi * df["time"].dt.dayofyear/365)    
+    df["day_sin"] = np.sin(2 * np.pi * df["time"].dt.dayofyear/365)
+    df["day_cos"] = np.cos(2 * np.pi * df["time"].dt.dayofyear/365)    
     
     return df
 
 def create_target(df:pd.DataFrame) -> pd.DataFrame:
+    
+    df["rain_next_hour"] = (df["precipitation"].shift(-1) >= 0.02).astype(int)
     
     return df
 
@@ -41,4 +43,4 @@ def run_feature_engineering(df:pd.DataFrame)-> pd.DataFrame:
             .pipe(create_target))
     
 df = run_feature_engineering(df.copy())
-print(df.head(10))
+print(df["rain_next_hour"].value_counts().head(50))
