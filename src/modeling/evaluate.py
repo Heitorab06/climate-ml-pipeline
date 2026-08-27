99db7bd2b0fbfdeaa547c32a72459e7c178476a7
Heitor Barreto <habarreto06@gmail.com>
2026-08-27 10:10:10 -0300

    feat: save metrics locally

--- src/modeling/evaluate.py
+++ src/modeling/evaluate.py
@@ -1,8 +1,7 @@
 from typing import Any
 import matplotlib.pyplot as plt
 import pandas as pd
-from src.modeling.train import prepare_model_dataset
-from src.modeling.train import train_models
+
 
 from sklearn.metrics import(
     average_precision_score,
@@ -11,7 +10,7 @@ from sklearn.metrics import(
     recall_score
     )
 
-def evaluate_models(models: dict[str, Any], data: dict[str, Any])-> dict[str, Any]:
+def evaluate_models(models: dict[str, Any], data: dict[str, Any])-> pd.DataFrame:
     results = []
 
     for name, model in models.items():
@@ -23,12 +22,16 @@ def evaluate_models(models: dict[str, Any], data: dict[str, Any])-> dict[str, An
         results.append({
             'Model': name,
             'PR-AUC': average_precision_score(data["y_test"], y_proba),
-            'F1_Score': f1_score(data['y_test'], y_proba),
+            'F1_Score': f1_score(data['y_test'], y_pred),
             'Recall': recall_score(data['y_test'], y_pred),
             'Precision': precision_score(data['y_test'], y_pred)
         })
 
-        return pd.DataFrame(results).sort_values(by='PR-AUC', ascending=False)
+    results_df = pd.DataFrame(results).sort_values(by='PR-AUC', ascending=False)
+    
+    #change this to save other models metrics
+    results_df.to_csv('results/evaluation_results_v1.csv', index=False)
+    return results_df
 
 def generate_graphics(metrics: pd.DataFrame) -> None:
     
